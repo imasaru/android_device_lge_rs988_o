d309f138855facf7bc61cfb7d6a4eb66f4a4d7a2
imasaru <evanmr4@gmail.com>
2018-08-09 20:26:42 -0500

    rs988: Fixes to /vendor partition support
    Updated checks and conditional operators, fixed carryover from V20 conversion
    
    Change-Id: Ide426af30e0b50b0bb19a497f65079b81afe45f7

--- recovery/root/vendor.sh
+++ recovery/root/vendor.sh
@@ -1,8 +1,8 @@
 #!/sbin/ash
 if [ `blkid /dev/block/bootdevice/by-name/vendor | grep -c ext4` -ge 1 ] ; then
-	echo '/dev/block/bootdevice/by-name/vendor	/vendor	ext4	ro	wait'>>/fstab.elsa
+	echo '/dev/block/bootdevice/by-name/vendor	/vendor	ext4	ro	wait'>>/fstab.h1
 	if [ ! -d /vendor ]; then # if not a directory, it either doesnt exist or is a symlink
-		if [ -h /vendor ]; then
+		if [ -L /vendor ]; then
 			rm /vendor
 		fi
 	fi
