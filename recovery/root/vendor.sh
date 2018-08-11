982a38ce7861442f87790c3c57d129df904a6e78
Phoenix591 <11486+Phoenix591@users.noreply.github.com>
2018-08-11 15:47:25 -0500

    rs988: Enable backup and image flashing of /vendor partition
    Change-Id: I111d2d8991fadd3896876b2f3f990f135629b168

--- recovery/root/vendor.sh
+++ recovery/root/vendor.sh
@@ -1,6 +1,7 @@
 #!/sbin/ash
 if [ `blkid /dev/block/bootdevice/by-name/vendor | grep -c ext4` -ge 1 ] ; then
-	echo '/dev/block/bootdevice/by-name/vendor	/vendor	ext4	ro	wait'>>/fstab.h1
+	echo '/vendor /dev/block/bootdevice/by-name/vendor	ext4	flags=backup;wipeingui'>>/etc/recovery.fstab
+	echo '/vendor_image /dev/block/bootdevice/by-name/vendor	emmc	flags=backup=0;flashimg'>>/etc/recovery.fstab
 	if [ ! -d /vendor ]; then # if not a directory, it either doesnt exist or is a symlink
 		if [ -L /vendor ]; then
 			rm /vendor
